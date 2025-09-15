39c693182665908b588e911d0748cc87d4859af3
Lilan Manoj <lilanmanoj@gmail.com>
2025-09-15 13:33:10 +0530

    Apache index page added

--- src/index.php
+++ src/index.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-    <meta charset="UTF-8" />
-    <title>It Works</title>
-</head>
-<body>
-<?php
-    $txt = "<h1>It works!</h1>";
-    $arr = array('A', 'B', 'C');
-    $ass_arr = array('first' => "A", 'second' => "B", 'third' => "C");
-    
-    echo $txt;
-    print_r ($arr);
-    echo "<br>";
-    print_r ($ass_arr);
-
-    echo (nl2br("\nFirst element of arr = ".$arr[0]));
-?>
-</body>
+    <head>
+        <title>It works!</title>
+    </head>
+    <body>
+        <?php
+            echo "It works!";
+        ?>
+    </body>
 </html>
